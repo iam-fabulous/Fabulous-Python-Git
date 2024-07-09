4d2255691c2f1f72fd960eea63f4ea5607375a55
iam-fabulous <emmanuelebukainnah@gmail.com>
2024-07-09 17:02:19 +0100

    added files

---
+++ PythonClassWork/checkforevenoroddnumber.py
@@ -0,0 +1,7 @@
+number = int(input("Enter number to check if it's even or odd! ___"))
+
+if number % 2 == 0:
+	print('The number entered is an Even number')
+else:
+	print('The number entered is an odd number')
+	
---
+++ PythonClassWork/ifstatementwork.py
@@ -0,0 +1,15 @@
+age = int(input("Enter your age"))
+
+if age > 19 and age <= 45:
+	print('Your are eligible to make your own decisions')
+	print("hello")
+	print("TechBLazers")
+
+elif age >= 13 and <= 19 :
+	print('you are a teenager')
+	print('Hello')
+
+elif age > 0 and age < 13
+	print("you are still growing up")
+else:
+	print("you are still a child")
